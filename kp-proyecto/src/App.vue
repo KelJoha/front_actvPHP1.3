01f4d2d8187bd78209892f321a617649305e5b7a
KelJoha <kjprietop@itc.edu.co>
2025-02-27 20:52:33 +0000

    feat: Se crea el menú y se actualiza el código fuente

---
+++ kp-proyecto/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import MenuPrincipal from './components/Menu.vue'
+
+</script>
+
+
+<template>
+  <div class="container-fluid">
+
+  <MenuPrincipal>
+  </MenuPrincipal>
+  <HelloWorld>
+  </HelloWorld>
+  
+  </div>
+ 
+</template>
+
+<style scoped>
+
+</style>
